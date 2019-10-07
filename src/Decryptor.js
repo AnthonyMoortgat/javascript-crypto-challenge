4a859bbf564e73a063ed1fdfed2e511c8eef9d8b
AnthonyMoortgat <44052663+AnthonyMoortgat@users.noreply.github.com>
2019-10-07 09:21:54 +0200

    Decrypt ciphertext working Test correct

---
+++ src/Decryptor.js
@@ -0,0 +1,23 @@
+const nacl = require('libsodium-wrappers')
+
+beforeAll(async () => {
+    await nacl.ready
+})
+
+let cryptKey;
+
+module.exports = {
+    setKey: function (key)
+    {
+        cryptKey = key;
+    },
+
+    decrypt: async function (ciphertext, nonce)
+    {
+        if(cryptKey == null){
+            throw 'no key'
+        }
+
+        return nacl.crypto_secretbox_open_easy(ciphertext, nonce, cryptKey)
+    }
+};
